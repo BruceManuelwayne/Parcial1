deba24b90593766ed99af0ed4a2e958def7d1d9f
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-10-13 22:58:21 -0300

    entrega

--- Program.cs
+++ Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace Parcial1
 {
-    class Program
+    public class Program
     {
+        private static Presentismo _presentismo;
+
+        static Program()
+        {
+            _presentismo = new Presentismo();
+        }
         static void Main(string[] args)
         {
+            Preceptor preceptorActivo = _presentismo.GetPreceptorActivo();
+
+            DesplegarOpcionesMenu();
+            string opcionMenu = ""; // pedir el valor
+            switch (opcionMenu)
+            {
+                case "1":
+                    TomarAsistencia(preceptorActivo);
+                    break;
+                case "2":
+                    MostrarAsistencia();
+                    break;
+                case "X":
+                    // SALIR
+                    break;
+                default:
+                    break;
+            }
+        }
+        static void DesplegarOpcionesMenu()
+        {
+            Console.WriteLine("1) Tomar Asistencia");
+            Console.WriteLine("2) Mostrar Asistencia");
+            Console.WriteLine("X: Terminar");
+        }
+        static void TomarAsistencia(Preceptor p)
+        {
+            // Ingreso fecha
+            // Listar los alumnos
+            // para cada alumno solo preguntar si está presente
+            // agrego la lista de asistencia
+            // Error: mostrar el error y que luego muestre el menu nuevamente
+        }
+        static void MostrarAsistencia()
+        {
+            // ingreso fecha
+            // muestro el toString de cada asistencia
         }
     }
+
 }
